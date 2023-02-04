2b668d703d15baeafb2b8d8a2c230f73fda9c105
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2023-02-04 02:16:36 -0500

    Zinguilini
    sin palabras yo lo se

--- GlobalGameJam_2023_Santiagos/Assets/Scripts/Zinguilini.cs
+++ GlobalGameJam_2023_Santiagos/Assets/Scripts/Zinguilini.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
+using UnityEngine;
 public class Zinguilini : MonoBehaviour
 {
     [SerializeField] private Transform [] Positions;
@@ -9,33 +10,91 @@ public class Zinguilini : MonoBehaviour
     [SerializeField] private float Speed= 0.1f;
     [SerializeField] private Transform LastPositions;
     [SerializeField] private int Position;
-
+    [SerializeField] private bool stop;
+    [SerializeField] private float time, stead;
+    Vector3 direction;
 
     // Start is called before the first frame update
     void Start()
     {
         ZinguiliniRb = GetComponent<Rigidbody2D>();
-      
+        stead = Speed * Time.deltaTime;
+        direction = transform.position - Positions[0].transform.position;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Positions.Length> Position)
+        //si hay posiciones de ir va 
+        if (!stop && Positions.Length!= (Position))
         {
-            transform.position = Vector3.MoveTowards(transform.position, Positions[Position].transform.position, Speed);
-            float a = Vector3.Distance(transform.position, Positions[Position].transform.position);
+
+           
+            transform.position = Vector3.MoveTowards(transform.position, Positions[Position].position, stead);
+            Vector3 newDirection = Vector3.RotateTowards(transform.forward, direction, stead, 0f);
+            transform.rotation = Quaternion.LookRotation(newDirection);
+            float a = Vector3.Distance(transform.position, Positions[Position].position);
             if (a==0)
             {
+               
                 Position++;
+                Debug.Log(Position); 
+                StartCoroutine(StopOneMoment());
+                if (Positions.Length != (Position))
+                {
+                    direction = transform.position - Positions[Position].position;
+                }
+                else
+                {
+                    direction = transform.position - LastPositions.position;
+                }
+                
+            }
+        }
+        //si ya llega a la finala
+        else if (!stop && Positions.Length==(Position))
+        {
+            transform.position = Vector3.MoveTowards(transform.position, LastPositions.position, stead);
+            float a = Vector3.Distance(transform.position, LastPositions.position);
+            if (a == 0)
+            {
+                StartCoroutine(ChageLoop());
             }
         }
        
        // Debug.Log(a+"faltantes");
     }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("coliciono");
     }
+ 
+    IEnumerator StopOneMoment()
+    {
+        stop = true;
+         Vector3 posicion = transform.position;
+ 
+        DOTween.Sequence()
+          .Append(transform.DOMove(transform.position + new Vector3(Random.Range(0.1f, 0.1f), Random.Range(0.1f, 0.1f), 0), 0.5f))
+           .Append(transform.DOMove(posicion, 0.5f)) ;
+           
+        yield return new WaitForSeconds(time);
+      
+        stop = false;
+
+    }
+    IEnumerator ChageLoop()
+    {
+        float starSpeed = stead;
+        stop = true;
+        Vector3 posicion = transform.position;
+        Position--;
+        yield return new WaitForSeconds(time/2);
+
+        stop = false;
+
+    }
+
 }
