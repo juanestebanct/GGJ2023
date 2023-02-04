e9be1436afb594d3b052639ae0f9927c2d56b607
SimonOsorio <soranokuroi@gmail.com>
2023-02-04 11:34:17 -0500

    Minimap navigation setup

--- GlobalGameJam_2023_Santiagos/Assets/Scripts/CameraManager.cs
+++ GlobalGameJam_2023_Santiagos/Assets/Scripts/CameraManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -5,19 +6,36 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    [SerializeField] private GameObject minimap;
+    [SerializeField] private GameObject minimapCamera;
+    [SerializeField] private GameObject openMinimapBtn;
     [SerializeField] private GameObject[] cameras = new GameObject[2];
+
+    private void Awake()
+    {
+        minimap.SetActive(true); minimapCamera.SetActive(true); openMinimapBtn.SetActive(false);
+    }
 
     public void ChangeCamera(int camera)
     {
         if (camera == 1)
         {
-            cameras[0].SetActive(false); cameras[1].SetActive(true);
+            cameras[0].SetActive(false); cameras[1].SetActive(true); minimap.SetActive(false);
+            minimapCamera.SetActive(false); openMinimapBtn.SetActive(true);
         }
 
         if (camera == 0)
         {
-            cameras[1].SetActive(false); cameras[0].SetActive(true);
+            cameras[1].SetActive(false); cameras[0].SetActive(true); minimap.SetActive(false);
+            minimapCamera.SetActive(false); openMinimapBtn.SetActive(true);
         }
+    }
+
+    public void OpenMinimap()
+    {
+        minimap.SetActive(true);
+        minimapCamera.SetActive(true);
+        openMinimapBtn.SetActive(false);
     }
     
     
