642705bd8a757c8f4aff7c29d2d490f6702cc333
SimonOsorio <soranokuroi@gmail.com>
2023-02-05 15:27:02 -0500

    Level 2

--- GlobalGameJam_2023_Santiagos/Assets/Scripts/LevelManager.cs
+++ GlobalGameJam_2023_Santiagos/Assets/Scripts/LevelManager.cs
@@ -11,7 +11,8 @@ public class LevelManager : MonoBehaviour
     public static LevelManager Instance;
     [SerializeField] private int currentLevel = 1;
     
-    [SerializeField] private RootController[] level1Roots = new RootController[2];
+    [SerializeField] private RootController[] level1Roots = new RootController[3];
+    [SerializeField] private RootController[] level2Roots = new RootController[4];
     [SerializeField] private float levelTime;
     [SerializeField] private bool[] HaveRootSpawned = new bool[5];
     private float timer;
@@ -32,10 +33,9 @@ public class LevelManager : MonoBehaviour
     void Start()
     {
         levelEnded = false;
-        if (currentLevel == 1)
-        {
-            StartCoroutine(Level1_RootManager());
-        }
+        if (currentLevel == 1) StartCoroutine(Level1_RootManager());
+        if (currentLevel == 2) StartCoroutine(Level2_RootManager());
+        
     }
 
     void Update()
@@ -70,6 +70,31 @@ public class LevelManager : MonoBehaviour
         level1Roots[2].GetComponent<Animator>().SetTrigger("play");
         
         StartCoroutine(GenerateEnemies());
+    }
+    
+    public IEnumerator Level2_RootManager()
+    {
+        levelTime = 120;
+        timer = 0;
+        foreach (var root in level1Roots) root.gameObject.SetActive(false);
+        yield return new WaitForSeconds(5f);
+        level2Roots[0].gameObject.SetActive(true);
+        HaveRootSpawned[0] = true;
+        level2Roots[0].GetComponent<Animator>().SetTrigger("play");
+        yield return new WaitForSeconds(10f);
+        level2Roots[1].gameObject.SetActive(true);
+        HaveRootSpawned[1] = true;
+        level2Roots[1].GetComponent<Animator>().SetTrigger("play");
+        yield return new WaitForSeconds(15f);
+        level2Roots[2].gameObject.SetActive(true);
+        HaveRootSpawned[2] = true;
+        level2Roots[2].GetComponent<Animator>().SetTrigger("play");
+        yield return new WaitForSeconds(20f);
+        level2Roots[3].gameObject.SetActive(true);
+        HaveRootSpawned[3] = true;
+        level2Roots[3].GetComponent<Animator>().SetTrigger("play");
+        
+        StartCoroutine(GenerateEnemies2());
     }
 
     public IEnumerator GenerateEnemies()
@@ -112,11 +137,76 @@ public class LevelManager : MonoBehaviour
             }
         }
     }
+    
+    public IEnumerator GenerateEnemies2()
+    {
+        while (!levelEnded)
+        {
+            if (HaveRootSpawned[0])
+            {
+                if (_generationZiguilinis[0].EnemyInstances <= 0) EnemiesManager.Instance.HideAlert(0);
+                Random random = new Random();
+                float rnd = random.Next(5, 15);
+                yield return new WaitForSeconds(rnd);
+                //Spawn Enemies
+                _generationZiguilinis[0].CanSpawn = true;
+                _generationZiguilinis[0].EnemyInstances = 5;
+                EnemiesManager.Instance.ShowAlert(0);
+            }
+            if (HaveRootSpawned[1])
+            {
+                if (_generationZiguilinis[1].EnemyInstances <= 1) EnemiesManager.Instance.HideAlert(1);
+                Random random = new Random();
+                float rnd = random.Next(5, 15);
+                yield return new WaitForSeconds(rnd);
+                //Spawn Enemies
+                _generationZiguilinis[1].CanSpawn = true;
+                _generationZiguilinis[1].EnemyInstances = 5;
+                EnemiesManager.Instance.ShowAlert(1);
+            }
+            if (HaveRootSpawned[2])
+            {
+                if (_generationZiguilinis[2].EnemyInstances <= 2) EnemiesManager.Instance.HideAlert(2);
+                Random random = new Random();
+                float rnd = random.Next(5, 15);
+                yield return new WaitForSeconds(rnd);
+                //Spawn Enemies
+                //Spawn Enemies
+                _generationZiguilinis[2].CanSpawn = true;
+                _generationZiguilinis[2].EnemyInstances = 5;
+                EnemiesManager.Instance.ShowAlert(2);
+            }
+            if (HaveRootSpawned[3])
+            {
+                if (_generationZiguilinis[3].EnemyInstances <= 3) EnemiesManager.Instance.HideAlert(3);
+                Random random = new Random();
+                float rnd = random.Next(5, 10);
+                yield return new WaitForSeconds(rnd);
+                //Spawn Enemies
+                //Spawn Enemies
+                _generationZiguilinis[3].CanSpawn = true;
+                _generationZiguilinis[3].EnemyInstances = 8;
+                EnemiesManager.Instance.ShowAlert(3);
+            }
+        }
+    }
 
     private void EndLevel(int completedLevel)
     {
         Debug.Log("Completado el nivel " + completedLevel);
         levelEnded = true;
+        if (completedLevel == 1)
+        {
+            SceneManager.LoadScene("Level 1 Outro");
+        }
+        else if (completedLevel == 2)
+        {
+            SceneManager.LoadScene("Level 3 Intro");
+        }
+        else if (completedLevel == 3)
+        {
+            SceneManager.LoadScene("Credits");
+        }
     }
 
     public void Lose()
