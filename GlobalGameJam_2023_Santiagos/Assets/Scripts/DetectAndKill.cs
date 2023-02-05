bab3e06f4451bdaa6ef566c526f8c39ff554c5fa
juanestebanct <78618669+juanestebanct@users.noreply.github.com>
2023-02-05 03:16:16 -0500

    prueba

--- GlobalGameJam_2023_Santiagos/Assets/Scripts/DetectAndKill.cs
+++ GlobalGameJam_2023_Santiagos/Assets/Scripts/DetectAndKill.cs
@@ -4,43 +4,36 @@ using UnityEngine;
 
 public class DetectAndKill : MonoBehaviour
 {
-    Queue<Collider2D> queue = new Queue<Collider2D>();
+    public Collider2D[] arrayOverlap;
     public LayerMask Enemy;
-    private bool destroy =false;
     public float range;
     // Start is called before the first frame update
     void Start()
     {
-     
+
     }
 
     private void OnDrawGizmosSelected()
     {
-        
+
 
         Gizmos.DrawSphere(transform.position, range);
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        Debug.Log("entra a la colision");
-        if (collision.CompareTag("Player") && Input.GetKeyDown(KeyCode.Space) && !destroy)
+        arrayOverlap = Physics2D.OverlapCircleAll(transform.position, range, Enemy);
+        if (collision.gameObject.tag == "Player" && Input.GetKeyDown("space"))
         {
-            destroy = true;
-           // Debug.Log("bum");
-            DesTroyEnemy();
-            
+            foreach (Collider2D col in arrayOverlap)
+            {
+                col.gameObject.GetComponent<Zinguilini>().Dead();
+             
+            }
         }
     }
     private void DesTroyEnemy()
     {
-        Collider2D [] arrayOverlap = Physics2D.OverlapCircleAll(transform.position, range, Enemy);
-        List<Collider2D> list = new List<Collider2D>();
-        list.AddRange(arrayOverlap);
-        list.ForEach(o => queue.Enqueue(o));
-        
-        queue.Dequeue().GetComponent<Zinguilini>().Dead();
-        
-        destroy = false;
+
     }
 
 }
