f1784c8bdbc1fbeb6ead9e54e2e6603a76459915
SimonOsorio <soranokuroi@gmail.com>
2023-02-17 00:42:22 -0500

    Loading Screen Progress

--- GlobalGameJam_2023_Santiagos/Assets/Scripts/Scene Management/IntroSceneManager.cs
+++ GlobalGameJam_2023_Santiagos/Assets/Scripts/Scene Management/IntroSceneManager.cs
@@ -8,23 +8,41 @@ public class IntroSceneManager : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
     [SerializeField] private float duration;
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private Animator branchAnimator;
+    [SerializeField] private bool nextLevelIsNight;
     private void Start()
     {
         Time.timeScale = 1;
+        loadingScreen.SetActive(false);
         StartCoroutine(LoadScene());
     }
 
     IEnumerator LoadScene()
     {
         yield return new WaitForSeconds(duration);
-        SceneManager.LoadScene(sceneToLoad);
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        loadingScreen.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        branchAnimator.SetTrigger("Grow");
+        if (nextLevelIsNight)
+        {
+            
+        }
+        yield return new WaitForSeconds(1f);
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        while (!loadOperation.isDone) yield return null;
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Escape))
         {
-            SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(LoadSceneAsync());
         }
     }
 }
